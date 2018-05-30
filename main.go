a5549c6423eadaef798fd44e98d1849cf66dd509
Georges <georges.colbert@rhsmith.umd.edu>
2018-05-30 16:48:19 -0400

    second test html display

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Hello World")
+}
 
 
 func main() {
-
-fmt.Println("Hello World")
+	http.HandleFunc("/", helloWorld)
+    http.ListenAndServe(":8080", nil)
 
 }
 
